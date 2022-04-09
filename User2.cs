43b62c4cf9afec81d78aef0e612c13cad9b6e185
SIMCON-VM\Creen <Milo Stankovi>
2022-04-09 07:37:18 +0200

    Prvi push dodata klasa Class1

---
+++ User2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitVezba
+{
+    class User2
+    {
+
+        public int User2Id { get; set; }
+        public string User2FirstName { get; set; }
+        public string User2LastName { get; set; }
+    }
+}
