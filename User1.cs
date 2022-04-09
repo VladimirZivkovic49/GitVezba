78bc325179a6a854a8c46258bd7692e78661780d
SIMCON-VM\Creen <Milo Stankovi>
2022-04-09 07:04:40 +0200

    Uvedana clasa Class1

---
+++ User1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+    public int User1Id { get; set; }
+    public string  User1FirstName { get; set; }
+    public string User1LastName { get; set; }
+
+
+}
+}
