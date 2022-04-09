b84bdbfb8f49bd5f2a2e60d2f85c6f14df6103f4
SIMCON-VM\Creen <Milo Stankovi>
2022-04-09 13:11:08 +0200

    Dodata klasa User 5

---
+++ User5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitVezba
+{
+    class User5
+    {
+        public int User5Id { get; set; }
+        public string User5FirstName { get; set; }
+        public string User5LastName { get; set; }
+
+
+
+    }
+}
